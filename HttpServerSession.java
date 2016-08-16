9c313e557f1771db85e367ba8f6402b13e25c8bf
Amarjot <amarjot-parmar@hotmail.co.nz>
2016-08-16 13:35:23 +1200

    Task 3.4 DONE ( Extract the filename)

--- HttpServerSession.java
+++ HttpServerSession.java
@@ -17,7 +17,31 @@ class HttpServerSession extends Thread
         {
             BufferedReader reader = new BufferedReader(new InputStreamReader(AcceptedSocket.getInputStream()));
             String FirstLine = reader.readLine();
-            System.out.println(FirstLine);
+            String parts[] = FirstLine.split(" ");
+
+            if(parts.length == 3)
+            {
+                if(parts[0].compareTo("GET") == 0)
+                {
+                    String filename = parts[1].substring(1);
+                    System.out.println(filename);
+                }
+            }
+
+            //Going though all the other lines until empty
+            while(true)
+            {
+                String line = reader.readLine();
+                if(line == null)
+                {
+                    //DONT KNOW WHAT TO DO HERE
+                }
+                if(line.compareTo("") == 0)
+                {
+                    //Line is empty
+                    break;
+                }
+            }
         }
         catch(Exception e)
         {
